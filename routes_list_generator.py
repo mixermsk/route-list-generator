4853e36c3de5d10b79291d0f9866a2bc0bc5a5f5
Andrey Roslyakov <a.roslyakov@postgrespro.ru>
2024-09-25 21:46:41 +0300

    Add source name in output

--- routes_list_generator.py
+++ routes_list_generator.py
@@ -12,33 +12,34 @@ import yaml
 
 class NetList():
     def __init__(self):
-        self.networks = set()
+        self.networks = {}
 
-    def add(self, cidr: str):
+    def add(self, block: str, cidr: str):
         new_net = ipaddress.ip_network(cidr)
-        for net in self.networks:
-            if net.num_addresses > 1 and new_net.subnet_of(net):
-                return
 
-            if new_net.num_addresses > 1 and new_net.supernet_of(net):
-                self.networks.add(new_net)
-                self.networks.remove(net)
-                return
+        if block not in self.networks:
+            self.networks[block] = set()
 
-        self.networks.add(new_net)
+        for _block, _networks in self.networks.items():
+            for _net in _networks:
+                if _net.num_addresses > 1 and new_net.subnet_of(_net):
+                    return
 
-    def __iter__(self):
-        for net in self.networks:
-            yield net
+                if new_net.num_addresses > 1 and new_net.supernet_of(_net):
+                    self.networks[block].add(new_net)
+                    _networks[_block].remove(_net)
+                    return
+
+        self.networks[block].add(new_net)
 
     def add_as(self, as_name: str):
         res = json.loads(subprocess.check_output(f'bgpq4 -j {as_name}'.split()))
         for prefix in [e['prefix'] for e in res['NN']]:
-            self.add(prefix)
+            self.add(as_name, prefix)
 
     def add_fqdn(self, fqdn: str):
         for cidr in {f'{res[4][0]}/32' for res in socket.getaddrinfo(fqdn, None, socket.AF_INET)}:
-            self.add(cidr)
+            self.add(fqdn, cidr)
 
 
 if __name__ == '__main__':
@@ -63,14 +64,17 @@ if __name__ == '__main__':
             if '/' not in entry:
                 entry += '/32'
 
-            netlist.add(entry)
+            netlist.add(entry, entry)
             continue
 
-        if re.match(r'\w+\.\w+', entry):
+        if re.match(r'[\w-]+\.\w+', entry):
             netlist.add_fqdn(entry)
             continue
 
         print(f'Unknown entry type {entry}', file=sys.stderr)
 
-    for _net in netlist:
-        print(f'push "route {_net.network_address} {_net.netmask}"')
+    for _block, _networks in netlist.networks.items():
+        if _networks:
+            print(f'# {_block}')
+        for net in _networks:
+            print(f'push "route {net.network_address} {net.netmask}"')
